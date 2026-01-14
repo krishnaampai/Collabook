00f324327d166d71dd9d84f4ace5841187f7ccaa
Niranjana Ramesh <niranjanaramesh.xd@gmail.com>
2026-01-14 23:31:03 +0530

    Notebook page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/landing";
 import Login from "./pages/Login";
 import Explore from "./pages/Explore";
 import Dashboard from "./pages/Dashboard";
+import Notebook from "./pages/notebook";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/explore" element={<Explore />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/notebook" element={<Notebook />} />
+
 
     </Routes>
   );
 }
 
 export default App;
-
